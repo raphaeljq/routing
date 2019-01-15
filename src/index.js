c49b7c052cdd356f60e4bafb2f236f982c193824
raphaeljq <raphaeljq@gmail.com>
2019-01-15 12:14:59 -0300

    Fiz uma coisa aqui

--- src/index.js
+++ src/index.js
@@ -1,46 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import {
-  Route,
-  NavLink,
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
-import App from "./App";
-import Users from "./users";
-import Contact from "./contact";
-import Notfound from "./notfound";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import App from './App'
+import Users from './users'
+import Contact from './contact'
+import Notfound from './notfound'
 
 const routing = (
   <Router>
     <div>
       <ul>
         <li>
-          <NavLink exact actvieClassName="active" to="/">
-            Home
-          </NavLink>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <NavLink actvieClassName="active" to="/users">
-            Users
-          </NavLink>
+          <Link to="/users">Users</Link>
         </li>
         <li>
-          <NavLink actvieClassName="active" to="/contact">
-            Contact
-          </NavLink>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
       <hr />
       <Switch>
         <Route exact path="/" component={App} />
-        <Route path="/users" component={Users} />
+        <Route exact path="/users" component={Users} />
         <Route path="/contact" component={Contact} />
         <Route component={Notfound} />
       </Switch>
     </div>
   </Router>
-);
-
-ReactDOM.render(routing, document.getElementById("root"));
+)
+ReactDOM.render(routing, document.getElementById('root'))
